4d246a6cf9415ab4b4e8c6080dc461edf1790b46
braunguy <0xbraunguy@gmail.com>
2022-06-28 21:52:28 -0400

    gtag fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import *  as mc from 'use-mailchimp-form'
-import React, { ChangeEvent, useEffect, useState } from 'react';
-
+import * as mc from 'use-mailchimp-form'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
-
-
-	const fieldRef = React.useRef<HTMLInputElement>(null);
+	const fieldRef = React.useRef<HTMLInputElement>(null)
 
 	const scrollToBottom = () => {
-		fieldRef.current?.scrollIntoView({ behavior: "smooth" })
+		fieldRef.current?.scrollIntoView({ behavior: 'smooth' })
 	}
-
 
 	return (
 		<>
@@ -23,20 +19,22 @@ const Home: NextPage = () => {
 
 			<div className="pages-wrapper">
 				<div className="pages-container">
-
 					<div className="header-wrapper">
 						<h1 className="header-title">LULO</h1>
-
 					</div>
 
 					<div className="main-wrapper page1">
-						<div className='button-wrapper'>
+						<div className="button-wrapper">
 							<div className="btn-grad-wrapper">
-								<button id='ctcbtn' onClick={scrollToBottom} className='contact-button'>Contact</button>
+								<button id="ctcbtn" onClick={scrollToBottom} className="contact-button">
+									Contact
+								</button>
 							</div>
 							<div className="btn-separator" />
 							<div className="">
-								<button disabled={true} className='app-button'>Launching soon...</button>
+								<button disabled={true} className="app-button">
+									Launching soon...
+								</button>
 							</div>
 						</div>
 						<h1 className="page-title">LULO</h1>
@@ -93,9 +91,7 @@ const Home: NextPage = () => {
 
 						<div className="right-block">
 							<h2 className="page-subtitle">Unparalleled Risk Management</h2>
-							<h3 className="page-subtitle">
-								 Manage your credit risk with our in-house secondary markets platform
-							</h3>
+							<h3 className="page-subtitle">Manage your credit risk with our in-house secondary markets platform</h3>
 						</div>
 					</div>
 
@@ -103,7 +99,8 @@ const Home: NextPage = () => {
 						<div className="left-block">
 							<h2 className="page-subtitle">Lender tools</h2>
 							<h3 className="page-subtitle">
-								Parameterized loans and custom lending tools allows you to isolate your lending to specific groups or institutions
+								Parameterized loans and custom lending tools allows you to isolate your lending to specific groups or
+								institutions
 							</h3>
 						</div>
 						<div className="right-block">
@@ -115,16 +112,14 @@ const Home: NextPage = () => {
 
 					<div className="blocks-wrapper page7">
 						<div className="left-block">
-						<h2 className="page-subtitle">Powered by Solana</h2>
-
+							<h2 className="page-subtitle">Powered by Solana</h2>
 						</div>
-			
 
 						<div className="right-block">
-						<div className="img-wrapper">
+							<div className="img-wrapper">
 								<img src="solanaShape1.png"></img>
 							</div>
-					</div>
+						</div>
 					</div>
 					<div className="page8">
 						<div className="mailchimp-wrapper">
@@ -132,11 +127,10 @@ const Home: NextPage = () => {
 						</div>
 					</div>
 					<div className="footer-wrapper">
-					<h3 className="footer-title">LULO</h3>
-					<div className="copyright">Copyright © 2022. All Rights Reserved</div>
+						<h3 className="footer-title">LULO</h3>
+						<div className="copyright">Copyright © 2022. All Rights Reserved</div>
+					</div>
 				</div>
-				</div>
-
 			</div>
 		</>
 	)
@@ -144,118 +138,92 @@ const Home: NextPage = () => {
 
 export default Home
 
-
 export const Analytics = () => {
-	return (
-		<div dangerouslySetInnerHTML={innerHtml()} />
-	)
+	return <div dangerouslySetInnerHTML={innerHtml()} />
 }
 
 function innerHtml() {
 	return {
 		__html: `
 		
-		<!-- Global site tag (gtag.js) - Google Analytics -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=G-XS1PVRSNFS"></script>
 		<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
 
-		gtag('config', 'G-XS1PVRSNFS');
-		gtag('set', {'allow_google_signals', false});
+			gtag('config', 'G-XS1PVRSNFS');
+			gtag('set', {'allow_google_signals': false});
 		</script>
 		
-		`
-	   }
+		`,
+	}
 }
 
-
-function MailChimp({fieldRef}:{fieldRef: React.RefObject<HTMLInputElement>}) {
+function MailChimp({ fieldRef }: { fieldRef: React.RefObject<HTMLInputElement> }) {
 	const mcUrl = 'https://gmail.us12.list-manage.com/subscribe/post?u=1e1f2971d71a0cde63a8b8b51&amp;id=6f6efae571'
-	const {
-		loading,
-		error,
-		success,
-		message,
-		handleSubmit
-	  } = mc.useMailChimpForm(mcUrl);
+	const { loading, error, success, message, handleSubmit } = mc.useMailChimpForm(mcUrl)
 
-	//   const { fields, handleFieldChange } = mc.useFormFields({
-	// 	EMAIL: "",
-	//   });
-
-	  const handleFieldChange = (event: ChangeEvent<HTMLInputElement>) => {
+	const handleFieldChange = (event: ChangeEvent<HTMLInputElement>) => {
 		setEmailAddress(event.target.value)
-	  } 
+	}
 
-	  useEffect(() => {
-		console.log(loading, error, success, message)
-
-	  }, [loading, error, success, message]
-	  )
-
-	  const [errorMsg, setErrorMsg] = useState<string>("")
-	  const [emailAddress, setEmailAddress] = useState<string>("")
+	const [errorMsg, setErrorMsg] = useState<string>('')
+	const [emailAddress, setEmailAddress] = useState<string>('')
 
 	useEffect(() => {
 		const id = setTimeout(() => {
-				setErrorMsg('')
+			setErrorMsg('')
 		}, 3000)
 		return () => {
 			clearInterval(id)
 		}
 	}, [errorMsg])
 
-	  const submitClick = () => {
-		if (emailAddress == "" || emailAddress.indexOf("@") == -1) {
-			setErrorMsg("Please enter a valid email address")
+	const submitClick = () => {
+		if (emailAddress == '' || emailAddress.indexOf('@') == -1) {
+			setErrorMsg('Please enter a valid email address')
 			return
 		}
 
-		handleSubmit({EMAIL: emailAddress})
-	  }
+		handleSubmit({ EMAIL: emailAddress })
+	}
 
 	return (
-		<div ref={fieldRef} className='mc-outer'>
-			<div className='mc-subscribe' >
-				Subscribe to stay up to date on our latest announcements
+		<div ref={fieldRef} className="mc-outer">
+			<div className="mc-subscribe">Subscribe to stay up to date on our latest announcements</div>
+			<div className="mc-inner">
+				<div className="input-and-response">
+					<div className="input-wrapper">
+						<input
+							className="mc-input"
+							id="EMAIL"
+							aria-autocomplete="none"
+							autoFocus={false}
+							autoComplete={'false'}
+							autoCorrect={'false'}
+							type="email"
+							value={emailAddress}
+							placeholder="Email..."
+							onChange={handleFieldChange}
+						/>
+					</div>
+
+					<div className="mc-response">
+						{loading && <div className="mc-loading"> {loading} </div>}
+						{error && <div className="mc-error"> {message} </div>}
+						{errorMsg && <div className="mc-error"> {errorMsg} </div>}
+						{success && <div className="mc-success"> {message} </div>}
+					</div>
+				</div>
+				<div className="submit-col">
+					<div className="submit-grad-wrapper">
+						<button onClick={submitClick} className="app-button mc-button ">
+							Submit
+						</button>
+					</div>
+				</div>
 			</div>
-			<div className='mc-inner'>
-
-					<div className='input-and-response'>
-
-						<div className="input-wrapper">
-							<input className='mc-input'
-								id="EMAIL"
-								aria-autocomplete='none'
-								autoFocus={false}
-								autoComplete={'false'}
-								autoCorrect={'false'}
-								type="email"
-								value={emailAddress}
-								placeholder="Email..."
-								onChange={handleFieldChange}
-							/>
-
-						</div>
-
-						<div className='mc-response'>
-								{loading && <div className='mc-loading'> {loading} </div>}
-								{error && <div className='mc-error'> {message} </div>}
-								{errorMsg && <div className='mc-error'> {errorMsg} </div>}
-								{success && <div className='mc-success'> {message} </div>}
-						</div>
-					</div>
-					<div className='submit-col'>					
-						<div className="submit-grad-wrapper">
-							<button onClick={submitClick}  className='app-button mc-button '>Submit</button>
-						</div>
-					</div>
-
-
 		</div>
-
-	  </div>
-	) 
+	)
 }
